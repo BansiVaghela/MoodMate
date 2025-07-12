efb5ffe5aa19e3685831e68f41da1e1adf15e0e1
vaghelabansi <vaghelabansi0908@gmail.com>
2025-07-12 18:13:57 +0530

    Initial Commit

---
+++ MoodMate/MoodMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodMateApp.swift
+//  MoodMate
+//
+//  Created by Bansi Vaghela on 12/07/25.
+//
+
+import SwiftUI
+
+@main
+struct MoodMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
